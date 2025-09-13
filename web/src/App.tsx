96bde048cf1745a494d83f052e6899ad60d6c320
chamathsenanayake <chamath.senanayake@q4us.dev>
2025-09-13 22:02:09 +0530

    added navigation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Layout, Menu } from 'antd';
+import { useCallback } from 'react';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    useNavigate,
+    useLocation,
+    Link,
+} from "react-router-dom";
+const { Content, Footer } = Layout;
 function App() {
-  const [count, setCount] = useState(0)
+  const items = [
+    {
+      key: '/',
+      label: 'Job Description',
+    },
+    {
+      key: '/cv',
+      label: 'CV',
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <MainLayout items={items} />
+    </BrowserRouter>
+  );
+}
+
+function MainLayout({ items }: { items: { key: string; label: string }[] }) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const setView = useCallback((view: string) => {
+        navigate(view)
+    }, []);
+  return (
+    <Layout className="h-[100vh]">
+      <Layout.Header className={"flex flex-row items-center justify-start border-b border-solid border-x-gray-500 pl-3 pr-0 bg-white"}>
+          <div className="flex flex-row justify-between items-center ">
+              <div className="flex w-14 h-full items-center">
+                  <Link to="/">
+                      <img src='/logo.png' className="bg-transparent p-0" alt={"logo"} />
+                  </Link>
+              </div>
+              <div className="w-px h-9 border" />
+          </div>
+          <Menu
+              theme="light"
+              mode="horizontal"
+              // defaultSelectedKeys={['/job_description']}
+              items={items}
+              style={{ width: "100vw" }}
+              className="flex-auto user-menu"
+              onSelect={(e) => {
+                  setView(e.key)
+              }}
+              selectedKeys={[location.pathname]}
+          />
+      </Layout.Header>
+
+      <Content style={{ padding: '0 48px' }}>
+        <Routes>
+          <Route path="/" element={<>Job Description Page</>} />
+          <Route path="/cv" element={<>CV Page</>} />
+        </Routes>
+      </Content>
+
+      <Footer style={{ textAlign: 'center' }}>
+        CVision ©{new Date().getFullYear()} Created by QGENIUS
+      </Footer>
+    </Layout>
+  );
 }
 
 export default App
